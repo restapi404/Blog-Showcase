d485df69469cb62849a82721a9f7eeddbffa15f5
restapi <rithuprabhu23@gmail.com>
2025-08-30 19:07:23 +0530

    Updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ import { Typewriter } from "react-simple-typewriter";
 
 export default function Navbar() {
   return (
-    <nav className="bg-[#4A0000] dark:bg-[#0C0C24] text-white px-6 py-4 flex flex-col md:flex-row md:justify-between md:items-center shadow">
+    <nav className="bg-[#4A0000] dark:bg-[#0C0C24] text-white px-6 py-4 flex flex-col md:flex-row md:justify-between md:items-center shadow transition-colors duration-300">
       <div>
         <div className="text-2xl font-bold">
           <Link href="/">Blog Showcase</Link>
@@ -23,9 +23,9 @@ export default function Navbar() {
         </div>
       </div>
       <div className="space-x-4 mt-2 md:mt-0">
-        <Link href="/" className="hover:text-yellow-400 transition">Home</Link>
-        <Link href="/blogs" className="hover:text-yellow-400 transition">Blogs</Link>
-        <Link href="/about" className="hover:text-yellow-400 transition">About</Link>
+        <Link href="/" className="hover:text-yellow-400 transition-colors duration-200">Home</Link>
+        <Link href="/blogs" className="hover:text-yellow-400 transition-colors duration-200">Blogs</Link>
+        <Link href="/about" className="hover:text-yellow-400 transition-colors duration-200">About</Link>
         <DarkModeToggle/>
       </div>
     </nav>
